366c9386c02da05c9f4b01b0911ec0c8bf069bbc
Jesse Lind <Jesseclind@gmail.com>
2024-03-26 16:57:19 -0400

    build: add react-helmet

--- template/src/pages/Home/Home.tsx
+++ template/src/pages/Home/Home.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import './Home.scss'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>Home</title>
+        <link rel='canonical' href='' />
+      </Helmet>
+      <div>Home</div>
+    </>
+  )
 }
 
 export default Home
